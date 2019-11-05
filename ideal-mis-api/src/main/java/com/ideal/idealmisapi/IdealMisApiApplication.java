321752fdf8645f2427752bcc417fc1027c22a7da
yanxiaobin <yanxb@ideal.sh.cn>
2019-11-05 16:35:51 +0800

    dfsf

--- ideal-mis-api/src/main/java/com/ideal/idealmisapi/IdealMisApiApplication.java
+++ ideal-mis-api/src/main/java/com/ideal/idealmisapi/IdealMisApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdealMisApiApplication {
 
     public static void main(String[] args) {
+//12323123
         SpringApplication.run(IdealMisApiApplication.class, args);
     }
 
